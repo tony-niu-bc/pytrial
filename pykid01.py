6eecb3f4ad27a4229d28bec8dbc121a5b763f7b7
hjc-niu <niushengchao@gmail.com>
2023-09-05 18:25:31 -0700

    tintin's exercise about variable and mathematical calculation

---
+++ pykid01.py
@@ -0,0 +1,83 @@
+print("I love pizze!")
+print("pizze " * 40)
+print("yum " * 40)
+print("Burp!")
+
+print(7 * 24 * 60)
+
+print("My name is Tintin.")
+print("I was born 8, 24, 2012.")
+print("My favorite color is bule.")
+
+strMyTeacher = "Mr. Morton"
+print(strMyTeacher)
+
+strNumber = "5 + 3"
+print(strNumber)
+
+iFirstNumber = 5
+iSecondNumber = 3
+print(iFirstNumber + iSecondNumber)
+
+iThirdNumber = iFirstNumber + iSecondNumber
+print(iThirdNumber)
+
+strLongString =""" Sing a song of sixpence, apocket full of rye.
+Four and twenty blackbirds baked in a pie.
+When't that adainty dish to set before the king?"""
+print(strLongString)
+
+Mymother = '?'
+print(Mymother)
+
+iNumber = 7
+iNumber = iNumber
+iNumber = iNumber + 1
+print(iNumber)
+
+iNumber = 7
+iNumber = iNumber + 1
+print(iNumber)
+
+print(5 - 3)
+print(6 / 2)
+
+strMyName = "Tintin"
+strBoyName = strMyName
+print(strBoyName)
+
+iHisScore = 4
+iHisScore = iHisScore + 1
+print(iHisScore)
+
+print(5 / 5)
+
+print(6 - 5)
+
+if '5' == "5" :
+    print('be same')
+else:
+    print("be NOT same")
+
+iDaysPerWeek = 7
+iHoursPerDay = 24
+iMinutesPerHour = 60
+
+print(iDaysPerWeek * iHoursPerDay * iMinutesPerHour)
+
+iHoursPerDay = 26
+print(iDaysPerWeek * iHoursPerDay * iMinutesPerHour)
+  
+print(9999999999999999999.9999 * 99999999999999999999999.9999)
+
+c = 2.5e6
+d = 1.2e7
+print(c + d)
+
+f = 9.1e1
+g = 8.1e2
+print(f + g)
+
+a = 24
+print(a)
+print(float(a))
---
+++ pykid02.py
@@ -0,0 +1,6 @@
+c = 38.0
+d = int(c)
+print(c)
+print(d)
+
+print(0.1+0.2)
