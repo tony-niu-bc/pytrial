6add43630e57c747de7d81a3ba02e254044e009a
Tony <niushengchao@gmail.com>
2024-01-01 00:18:02 -0800

    have done three and four chapters

---
+++ pykid03.py
@@ -0,0 +1,20 @@
+print(9 // 5)
+
+print(9 / 5)
+
+print(6 ** 4)
+print(6 * 6 * 6 *6)
+
+print(1.7e7)
+
+print((35.27 + 35.27 * 0.15)/ 3)
+
+print(16.7 * 12.5)
+print(16.7 * 2 + 12.5 * 2)
+
+f = float (input("please input a fahrenheit: "))
+c = (5 / 9 )* (f - 32)
+print(c)
+
+print(200 / 80)
+
---
+++ pykid04.py
@@ -0,0 +1,18 @@
+f = float(input("please put a float:"))
+print(f)
+print(str(f))
+i = int(f)
+print(i)
+
+print("to round 13.2 and 13.7")
+print(int(13.2 + 0.5))
+print(int(13.7 + 0.5))
+
+print(float(12.54))
+
+print(int(56.99999999))
+
+print(int ("896"))
+
+print(0.1 + 0.2)
+
