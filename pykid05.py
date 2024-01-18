4cce4dc966f806dbdd2096db2a61947879a74528
hjc <niushengchao@gmail.com>
2024-01-17 23:05:59 -0800

    have done five chapters

---
+++ pykid05.py
@@ -0,0 +1,21 @@
+iNum = int(float(input("Enter your number: ")))
+print(iNum)
+print("integer: " + str(iNum))
+
+j = float(input("Enter your number: "))
+print(j)
+
+firstName = input("please input your firstName: ")
+lastName = input("please input your firstName: ")
+print(firstName + lastName)
+
+leangh = input("lengh is how long: ")
+width = input("width is how long: ")
+print(float(leangh) * float(width))
+print(float(leangh) * float(width) * 9)
+print(float(leangh) * float(width) * 5)
+ 
+d = input("please input you have how many dollar: ")
+t = input("please input you have how many ten cents: ")
+c = input("please input you have how many cens: ")
+print((((float(d) * 100) + (float(t) * 10 )) + float(c) )/ 100)
